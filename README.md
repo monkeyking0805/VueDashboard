a1da9bbaab840365c0d291d22198a85c1bb6dabb
Ono Sakia <luckyonosakia@gmail.com>
2022-03-07 10:28:53 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+![appstore](https://user-images.githubusercontent.com/46362478/84034458-c08b3000-a970-11ea-85cf-82072c0a3872.png)
+
+# Vue + Vuex + Electron + Tailwind
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run electron:serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app" style="-webkit-app-region: drag" class="font-sans flex min-h-screen">
+    <sidebar></sidebar>
+    <!-- <transition name="router-animation" enter-active-class="animated slideInUp faster"> -->
+    <router-view/>
+    <!-- </transition> -->
+  </div>
+</template>
+
+<script>
+import Sidebar from './components/Sidebar.vue'
+import 'animate.css/animate.css'
+
+export default {
+  components: {
+    Sidebar,
+  }
+}
+</script>
+
+
+<style src="./assets/main.css">
